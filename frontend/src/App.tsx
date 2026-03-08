81ce877c7d369ef4eec98cfbad15ab9d5e6f5efd
ryanabaldwin <ryanb24@byu.edu>
2026-03-08 16:54:29 -0600

    Final changes 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,53 @@
 import './App.css'
+import teamsData from "./CollegeBasketballTeams.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+type TeamType = {
+  school: string;
+  name: string; // mascot
+  city: string;
+  state: string;
+};
+
+function Team({ school, name, city, state}: TeamType) {
+  return (
+    <>
+      <h2>School: {school} {name}</h2>
+      <h3>Location: {city}, {state}</h3>
+    </>
+  );
+}
+
+function Welcome() {
+  return (
+    <>
+    <h1>College Basketball Teams</h1>
+    <h2>This site displays a list of all college basketball teams</h2>
+    </>
+    );
+}
+
+function TeamsList() {
+  const teams = teamsData.teams;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        teams.map((singleTeam) => (
+          <Team key={singleTeam.school} {...singleTeam} />
+        ))
+      }
     </>
-  )
+  );
+}
+
+
+function App() {
+  return (
+    <>
+      <Welcome />
+      <TeamsList />
+    </>
+  );
 }
 
 export default App
